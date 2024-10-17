8a60c74f4947d28dc4106e920d0481bee2ce766c
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2024-10-17 02:55:56 +0100

    event styling
    - data within the event have been positioned
    - important events will highlight the border

--- Calendar App/src/components/Event/Event.jsx
+++ Calendar App/src/components/Event/Event.jsx
@@ -5,11 +5,43 @@ import { useState } from 'react';
 function Event(props) {
   const [data, setData] = useState(props.data);
 
+  // Extract day from dates
+  const days = ["Sun", "Mon", "Wed", "Thu", "Fri", "Sat"];
+  var startDate = new Date(data.startDate);
+  var endDate = new Date(data.endDate);
+
+  startDate = days[startDate.getDay()];  
+  endDate = days[endDate.getDay()];
+
+  // Extract date from dates
+  const startDateDay = startDate + " " +  data.startDate.split("-")[2];
+  const endDateDay = endDate + " " +  data.endDate.split("-")[2];
+
+  // // Extract month from dates
+  // const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  // const startDateMonth = months[data.startDate.split("-")[1]];
+  // const endDateMonth = months[data.endDate.split("-")[1]];
+
   return (
-    <div id={"event_" + props.id} className='event-container'>
-        <p>{data.title}</p>
-        <p>{data.description}</p>
-        <p>{data.location}</p>
+    <div id={"event_" + props.id} className={(data.important) ? "event-container important" : "event-container"}>
+      <div className='date-container'>
+        <p className="date">{(data.startDate == data.endDate || data.allDay) ? startDateDay : startDateDay + "\n - \n" + endDateDay}</p>
+      </div>
+      
+      <div className='info-container'>
+        <h3 className="title">{data.title}</h3>
+        <p className="description">{data.description}</p>
+        <p className="location"><span className="material-symbols-outlined">pin_drop</span>{data.location} | 12:20 - 24:40</p>
+      </div>
+      
+      {/* <div className='time-container'>
+        <p className="time">{(!data.allDay) ? data.startTime + " - " + data.endTime : "All Day"}</p>
+      </div> */}
+
+      {/* <div className='controls-container'>
+        <button className='action-button'><span className="material-symbols-outlined">edit</span>Edit</button>
+        <button className='action-button'><span className="material-symbols-outlined">delete</span>Delete</button>
+      </div> */}
     </div>
   )
 }
--- Calendar App/src/components/EventList/EventsList.jsx
+++ Calendar App/src/components/EventList/EventsList.jsx
@@ -24,12 +24,50 @@ const EventData = class {
 
 function EventsList() {
   const [events, setEvents] = useState([
-    new EventData("0", "Test Event", "Description of the event")
+    new EventData(
+      "event_0",
+      "Title",
+      "Descriptiona aaaaaaaaaa aaaaaaa aaaaaaaaaaa aaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaa aaaaa aaaaa aaaaaa aaaaaaaaaaaa aaaaaa aaaaaa aaaaaaaaa aaaaa aaaaaaaaaaaa aaaa aaaaaa aaaaaaa aaaaaaa aaaaaaaa aaaaaaa aaaa aaaaaaaa aaaa aaaaaaaaa aaaaaa aaaaaaaaa aaaaaaaaa aaaaaaaaa aaaaaaaaaaaaaa aaaaaa aaa aaaaa aaaaaaa aaaaa aaaaaaaa aaaaa aaaaaaaa aaaaa aaaaa aaaaaaaa aaaaa aaaaaaaaaaaaa aaaaaa aaaaaaaaa aaaaaa aaaaaa aaaaaa aaaaaaa aaaaaaaaa aaaaaaaa aaaaaaa aaaaaa aaaaaaa aaaaa aaaa",
+      "Location",
+      "2024-06-24",
+      "2024-06-24",
+      "14:30",
+      "15:00",
+      false,
+      "white",
+      false
+    ),
+    new EventData(
+      "event_1",
+      "Title",
+      "Description",
+      "Location",
+      "2024-06-24",
+      "2024-06-27",
+      "13:25",
+      "18:45",
+      false,
+      "white",
+      true
+    ),
+    new EventData(
+      "event_2",
+      "Title",
+      "Description",
+      "Location",
+      "2024-07-11",
+      "2024-07-11",
+      "hh:mm",
+      "hh:mm",
+      true,
+      "white",
+      false
+    ),
   ]);
 
   // Event functions
   const addEvent = (title, description, location) => {
-    const newEvent = new EventData("0", title, description, location);
+    const newEvent = new EventData("event_" + events.length, title, description, location, "2024-01-01", "2024-02-02", "00:00", "01:10", false, "white", false);
 
     setEvents((prev) => ([...prev, newEvent]));
   }
@@ -47,6 +85,10 @@ function EventsList() {
       </div>
       <AddEventDialog addEvent={addEvent}/>
       <div className='events-container'>
+        <div>
+          <h2>June 2024</h2>
+          <hr />
+        </div>
         {events.map((event, index) => (
           <Event key={event.id} data={events[index]} />
         ))}
